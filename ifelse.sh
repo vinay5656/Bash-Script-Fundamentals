f90839661af0b6e4ad32f906b20aed7f3321d028
Vinay Singh Chouhan <vinaysinghchauhan5656@gmail.com>
2025-02-27 11:54:38 +0530

    made changes to ifelse.sh file

--- ifelse.sh
+++ ifelse.sh
@@ -4,6 +4,8 @@ Age=18
 
 if [ "$Age" -ge 18 ]; then
     echo "I can drive a car"
+    echo $(systeminfo) | cat >> "../../Downloads/sysinfo.txt"
+    cat "../../Downloads/sysinfo.txt"
 else 
     echo "I do not have permit to drive a car"
 fi
