610ace28ce53c51dee1b78c5925e55d02ab62858
Vinay Singh Chouhan <vinaysinghchauhan5656@gmail.com>
2025-02-27 11:51:02 +0530

    Revert "Added on more command to ifelse"
    This reverts commit 6a23a5afa9af282f9090caac251a531d2ce46910.

--- ifelse.sh
+++ ifelse.sh
@@ -4,8 +4,6 @@ Age=18
 
 if [ "$Age" -ge 18 ]; then
     echo "I can drive a car"
-    echo $systeminfo | cat >> "../../Downloads/index.txt"
-    cat "../../Downloads/index.txt"
 else 
     echo "I do not have permit to drive a car"
 fi
